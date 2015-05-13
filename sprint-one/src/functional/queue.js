a21e2a1bd92c8671ed41d2778110fd5718906b7b
danielleknudson <knudson.danielle@gmail.com>
2015-05-13 15:01:47 -0700

    Pass queue.js tests in functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,63 @@
+// var Queue = function(){
+//   var someInstance = {};
+
+//   // Use an object with numeric keys to store values
+//   var storage = {};
+
+//   // Implement the methods below
+
+//   someInstance.enqueue = function(value){
+//     var key = someInstance.size();
+//     storage[key] = value;
+//   };
+
+//   someInstance.dequeue = function(){
+//     var result = storage[0];
+//     var temp = {};
+//     delete storage[0];
+//     for(var i = 0; i<someInstance.size(); i++){
+//       temp[i] = storage[i + 1];
+//     }
+//     storage = temp;
+//     return result;
+//   };
+
+//   someInstance.size = function(){
+//     var size = 0;
+//     for(var key in storage){
+//       size++;
+//     }
+//     return size;
+//   };
+
+//   return someInstance;
+// };
+
 var Queue = function(){
   var someInstance = {};
 
-  // Use an object with numeric keys to store values
   var storage = {};
-
-  // Implement the methods below
+  var size = 0;
 
   someInstance.enqueue = function(value){
+    storage[size] = value;
+    size++;
   };
 
-  someInstance.dequeue = function(){
+  someInstance.dequeue = function(value){
+    var result = storage[0];
+    if (size > 0) {
+      for(var i = 0; i<size; i++){
+        storage[i - 1] = storage[i];
+      }
+      delete storage[-1];
+      size--;
+    }
+    return result;
   };
 
-  someInstance.size = function(){
+  someInstance.size = function(value){
+    return size;
   };
 
   return someInstance;
