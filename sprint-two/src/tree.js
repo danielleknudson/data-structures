1d4d53143807a3c6b0e4496f4b27f4d776dff4cf
danielleknudson <knudson.danielle@gmail.com>
2015-05-14 20:04:37 -0700

    Pass first four tests of the tree.js data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,26 +1,33 @@
 var Tree = function(value){
   var newTree = {};
+  _.extend(newTree, treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
 
 
 
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push({value: value});
 };
 
 treeMethods.contains = function(target){
+  for(var i = 0; i<this.children.length; i++){
+    if(this.children[i].value === target){
+      return true;
+    }
+  }
+  return false;
 
 };
+
+var myFamilyHistory = Tree("Our Family History");
 
 
 /*
