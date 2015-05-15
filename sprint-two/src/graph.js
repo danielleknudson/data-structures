08075145a93b468284af772f340c8717a19765fe
danielleknudson <knudson.danielle@gmail.com>
2015-05-15 15:59:45 -0700

    Pass graph.js tests and fix graphSpec.js tests

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,64 @@
 
 
 var Graph = function(){
+  this.vertices = [];
+  this.edges = [];
 };
 
 Graph.prototype.addNode = function(node){
+  this.vertices.push(node);
 };
 
 Graph.prototype.contains = function(node){
+  for (var i = 0; i < this.vertices.length; i++){
+    if (this.vertices[i] === node){
+      return true;
+    }
+  }
+  return false;
 };
 
 Graph.prototype.removeNode = function(node){
+  var result = [];
+
+  for (var i = 0; i < this.vertices.length; i++){
+    if (this.vertices[i] !== node){
+      result.push(this.vertices[i]);
+    }
+  }
+
+  this.vertices = result;
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  for (var i = 0; i < this.edges.length; i++){
+    if((this.edges[i].indexOf(fromNode) > -1) && (this.edges[i].indexOf(toNode) > -1)){
+      return true;
+    }
+  }
+  return false;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this.edges.push([fromNode, toNode]);
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  var result = [];
+
+  for (var i = 0; i < this.edges.length; i++){
+    if (!((this.edges[i].indexOf(fromNode) > -1) && (this.edges[i].indexOf(toNode) > -1))){
+      result.push(this.edges[i]);
+    }
+  }
+
+  this.edges = result;
 };
 
 Graph.prototype.forEachNode = function(cb){
+  for (var i = 0; i < this.vertices.length; i++){
+    cb(this.vertices[i]);
+  }
 };
 
 /*
