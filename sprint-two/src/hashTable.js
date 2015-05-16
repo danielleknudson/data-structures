1064cadfd4f51f8bbb45735c29e8d5817f78614d
danielleknudson <knudson.danielle@gmail.com>
2015-05-16 10:52:48 -0700

    Pass hashTable.js tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,3 +1,42 @@
+// var HashTable = function(){
+//   this._limit = 8;
+//   this._storage = LimitedArray(this._limit);
+// };
+
+// // add "Steven", "Seagal"
+// // getIndexBelowMaxForKey("Steven", this._limit) -> unique key
+
+// HashTable.prototype.insert = function(key, value){
+//   var i = getIndexBelowMaxForKey(key, this._limit);
+//   if(!Array.isArray(this._storage[i])){
+//     this._storage[i] = [];
+//   }
+//   this._storage[i].push([key, value]);
+
+//   console.log(this._storage);
+// };
+
+// HashTable.prototype.retrieve = function(key){
+//   var i = getIndexBelowMaxForKey(key, this._limit);
+
+//   for(var j =0; j<this._storage[i].length; j++){
+//     if(this._storage[i][j][0] === key){
+//       return this._storage[i][j][1];
+//     }
+//   }
+// };
+
+// HashTable.prototype.remove = function(key){
+//   var i = getIndexBelowMaxForKey(key, this._limit);
+
+//   for (var j = 0; j < this._storage[i].length; j++){
+//     if(this._storage[i][j][0] === key){
+//       this._storage[i][j][1] = null;
+//     }
+//   }
+
+// };
+
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -8,24 +47,42 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(key, value){
   var i = getIndexBelowMaxForKey(key, this._limit);
-  if(!Array.isArray(this._storage[i])){
-    this._storage[i] = [];
-  }
-  this._storage[i].push([key, value]);
 
-  console.log(this._storage);
+  // check if there's an array in the index
+  if (!Array.isArray(this._storage.get(i))){
+    this._storage.set(i, []);
+  }
+
+  this._storage.get(i).push({key: key, value: value});
 };
 
 HashTable.prototype.retrieve = function(key){
   var i = getIndexBelowMaxForKey(key, this._limit);
-  return this._storage[i];
+  var result;
+  this._storage.each(function(house, houseIndex){
+    if(houseIndex === i){
+      for(var j = 0; j < house.length; j++){
+        if(house[j].key === key){
+          result = house[j].value;
+        }
+      }
+    }
+  });
+  return result;
 };
 
 HashTable.prototype.remove = function(key){
   var i = getIndexBelowMaxForKey(key, this._limit);
-  this._storage[i] = null;
+  this._storage.each(function(house, houseIndex){
+    if(houseIndex === i){
+      for(var j = 0; j < house.length; j++){
+        if(house[j].key === key){
+          house[j].value = null;
+        }
+      }
+    }
+  });
 };
-
 
 
 /*
